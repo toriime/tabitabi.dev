26c2206ecbc2c04eb173ba968159377f8cf9e424
Shirone <kacperlachowiczwp.pl@wp.pl>
2023-11-01 23:18:33 +0100

    pretty

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,30 +1,28 @@
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-import '@/style/globals.css'
-import NavBar from '@/components/navbar'
-import Footer from '@/components/footer'
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "@/style/globals.css";
+import NavBar from "@/components/navbar";
+import Footer from "@/components/footer";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'TabTabi',
-  description: 'Oficjalna strona TabiTabi Development',
-}
+  title: "TabTabi",
+  description: "Oficjalna strona TabiTabi Development",
+};
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
+type Props = {
+  children: React.ReactNode;
+};
+
+export default function RootLayout({ children }: Props) {
   return (
     <html lang="en">
-      <body className={inter.className + 'h-full'}>
+      <body className={inter.className + "h-full"}>
         <NavBar />
-        <main className="mt-[80px]">
-          {children}
-        </main>
+        <main className="mt-[80px]">{children}</main>
         <Footer />
       </body>
     </html>
-  )
+  );
 }
