5425611baef0d79b0099f29a873a20c215ef6809
Shirone <kacperlachowiczwp.pl@wp.pl>
2023-11-01 23:53:57 +0100

    about page

--- next.config.js
+++ next.config.js
@@ -1,14 +1,22 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        remotePatterns: [{
-            hostname: "avatars.githubusercontent.com"
-        }, {
-            hostname: "fredqah54zct.objectstorage.eu-frankfurt-1.oci.customer-oci.com"
-        }, {
-            hostname: "objectstorage.eu-frankfurt-1.oraclecloud.com"
-        }]
-    }
-}
+  images: {
+    remotePatterns: [
+      {
+        hostname: "avatars.githubusercontent.com",
+      },
+      {
+        hostname:
+          "fredqah54zct.objectstorage.eu-frankfurt-1.oci.customer-oci.com",
+      },
+      {
+        hostname: "objectstorage.eu-frankfurt-1.oraclecloud.com",
+      },
+      {
+        hostname: "cdn.discordapp.com",
+      },
+    ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,7 +1,48 @@
+import Authors from "@/authors.json";
+import MaxWidthWrapper from "@/components/max-width-wrapper";
+import Image from "next/image";
+
 export default function BlogFrontPage() {
-    return (
-        <>
-            Dla Malezzji
-        </>
-    )
+  return (
+    <MaxWidthWrapper>
+      <div className="max-w-[1300px] flex flex-col gap-2 items-center mb-10 p-8">
+        <h1 className="mb-4 text-3xl font-bold">O nas</h1>
+        TabiTabi Development to grupa developerów, którzy tworzą projekty
+        związane z anime. <br /> Poniżej znajdziesz developerów, którzy pracują
+        nad projektem.
+        <div
+          className={"mt-3 flex flex-wrap items-center justify-center gap-6"}
+        >
+          {Authors.map((user, i) => (
+            <div
+              key={i}
+              className={"flex flex-col items-center justify-center relative"}
+            >
+              <Image
+                src={user.avatar}
+                alt={"user avatar"}
+                className={"avatar-border h-20 w-20 rounded-full"}
+                width={200}
+                height={200}
+              />
+              {user.username === "sivy" && (
+                <Image
+                  src="/sivy.jpg"
+                  alt={"sivy toys"}
+                  className={
+                    "avatar-border h-20 w-20 rounded-full absolute top-3 left-10"
+                  }
+                  width={200}
+                  height={200}
+                />
+              )}
+              <h3 className={"mt-1 text-center text-lg font-bold"}>
+                {user.username}
+              </h3>
+            </div>
+          ))}
+        </div>
+      </div>
+    </MaxWidthWrapper>
+  );
 }
