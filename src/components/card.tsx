7589305a2330caa05fd66aaf7c7c1b9de8dca422
malezjaa <66274761+malezjaa@users.noreply.github.com>
2023-11-02 18:01:16 +0100

    test

--- src/components/card.tsx
+++ src/components/card.tsx
@@ -1,65 +1,71 @@
-import React from 'react'
-import Image from 'next/image'
-import Link from 'next/link'
+import React from "react";
+import Image from "next/image";
+import Link from "next/link";
 
 interface CardProps {
-    title: string
-    authors: {
-        username: string
-        avatar: string
-    }[]
-    image: string
-    link: string
-    date: Date
-    tags: string[]
+  title: string;
+  authors: {
+    username: string;
+    avatar: string;
+  }[];
+  image: string;
+  link: string;
+  date: Date;
+  tags: string[];
 }
 
 interface AuthorsProps {
-    authors: {
-        username: string
-        avatar: string
-    }[]
+  authors: {
+    username: string;
+    avatar: string;
+  }[];
 }
 
 const Card = ({ title, image, link, tags, date, authors }: CardProps) => {
-    return (
-        <Link href={link}>
-            <div className="flex flex-col gap-3 items-start rounded-lg border-slate-600/80 hover:bg-slate-300/5 transition-colors duration-200 border shadow-sm w-full overflow-hidden">
-                <div className="w-full">
-                    <Image src={image} alt="post image" width={1920} height={1080} />
-                </div>
-                <div className="flex flex-col p-3 gap-2 w-full">
-                    <div className="flex flex-wrap gap-2">
-                        {tags.map((tag, index) => (
-                            <span key={index} className="text-slate-300/60 text-sm rounded-full bg-slate-300/5 py-1 px-2 border-slate-600/80 border">
-                                {tag}
-                            </span>
-                        ))}
-                    </div>
-                    <h1 className="w-full text-start mt-1">{title}</h1>
-                    <span className="flex justify-between mt-3">
-                        <CardAuthors authors={authors} />
-                        <span className="text-slate-300/60">
-                            {date.toLocaleDateString()}
-                        </span>
-                    </span>
-                </div>
-            </div>
-        </Link>
-    )
-}
+  return (
+    <Link href={link}>
+      <div className="flex flex-col gap-3 items-start rounded-lg border-slate-600/80 hover:bg-slate-300/5 transition-colors duration-200 border shadow-sm w-full overflow-hidden">
+        <div className="w-full">
+          <Image src={image} alt="post image" width={1920} height={1080} />
+        </div>
+        <div className="flex flex-col p-3 gap-2 w-full">
+          <div className="flex flex-wrap gap-2">
+            {tags.map((tag, index) => (
+              <span
+                key={index}
+                className="text-slate-300/60 text-sm rounded-full bg-slate-300/5 py-1 px-2 border-slate-600/80 border"
+              >
+                {tag}
+              </span>
+            ))}
+          </div>
+          <h1 className="w-full text-start mt-1">{title}</h1>
+          <span className="flex justify-between mt-3">
+            <CardAuthors authors={authors} />
+            <span className="text-slate-300/60">
+              {date.toLocaleDateString()}
+            </span>
+          </span>
+        </div>
+      </div>
+    </Link>
+  );
+};
 
 const CardAuthors = ({ authors }: AuthorsProps) => (
-    <div className="w-28 h-7 flex items-center flex-shrink-0 relative">
-        {authors.map((member, index) => (
-            <Image 
-                key={member.username} src={member.avatar} 
-                className="rounded-full w-7 h-7 absolute" 
-                style={{ left: `${index * 1.1}rem`, zIndex: authors.length + index }}
-                alt="author avatar" width={30} height={30} />
-        ))}
-    </div>
-)
+  <div className="w-28 h-7 flex items-center flex-shrink-0 relative">
+    {authors.map((member, index) => (
+      <Image
+        key={member.username}
+        src={member.avatar}
+        className="rounded-full w-7 h-7 absolute"
+        style={{ left: `${index * 1.1}rem`, zIndex: authors.length + index }}
+        alt="author avatar"
+        width={30}
+        height={30}
+      />
+    ))}
+  </div>
+);
 
-
-export default Card
+export default Card;
