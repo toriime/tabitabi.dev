03f3b10cc31509cb5a96366e030013538055eb1c
Shirone <kacperlachowiczwp.pl@wp.pl>
2023-12-06 19:27:50 +0100

    [feat] top loader

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "@/style/globals.css";
 import NavBar from "@/components/navbar";
 import Footer from "@/components/footer";
+import NextTopLoader from "nextjs-toploader";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,6 +20,18 @@ export default function RootLayout({ children }: Props) {
   return (
     <html lang="en">
       <body className={inter.className + "h-full"}>
+        <NextTopLoader
+          color="#f48fb1"
+          initialPosition={0.08}
+          crawlSpeed={200}
+          height={3}
+          crawl={true}
+          showSpinner={true}
+          easing="ease"
+          speed={200}
+          zIndex={1600}
+          showAtBottom={false}
+        />
         <NavBar />
         <main className="mt-[80px] mb-10">{children}</main>
         <Footer />
