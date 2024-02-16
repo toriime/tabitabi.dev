aa7e49b0ef343383822b34a94502a6494cc53965
rezizdigus <rezizdiguspl@gmail.com>
2024-02-16 23:30:44 +0100

    wyśrodkowanie navbara

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -47,43 +47,48 @@ const NavBar = () => {
 
   return (
     <nav
-      className={`max-w-50 z-50 fixed top-0 left-0 w-full h-20 py-4 px-8 outline outline-1 outline-b outline-borderLight bg-slate-600/5 flex items-center transition-all ${
+      className={`max-w-50 z-50 fixed top-0 left-0 w-full h-20 py-4 px-8 outline outline-1 outline-b outline-borderLight bg-slate-600/5 transition-all ${
         scrolled ? "!outline-0 bg-transparent" : ""
       }`}
     >
-      <h1
-        className={`transition-all duration-300 ${
-          scrolled ? "-translate-x-4 opacity-0" : ""
-        } hidden sm:block h-full`}
-      >
-        <Image src="/wordmark.svg" className="" alt="TabiTabi" width={80} height={0} />
-      </h1>
-      <div
-        ref={navRef}
-        className={`max-w-50 m-auto h-full rounded-full border border-borderLight p-1 flex relative group transition-all ${
-          scrolled ? "shadow-md backdrop-blur bg-slate-600/10" : ""
-        }`}
-      >
-        <div className="opacity-0 group-hover:opacity-100 absolute w-full h-full top-0 left-0 p-1 -translate-x-1 transition-opacity">
+      <div className="relative w-full flex items-center">
+        <h1
+          className={`transition-all duration-300 ${
+            scrolled ? "-translate-x-4 opacity-0" : ""
+          } hidden sm:block h-full`}
+        >
+          <Image src="/wordmark.svg" className="" alt="TabiTabi" width={80} height={0} />
+        </h1>
+        <div className="absolute w-full flex items-center top-0 left-0">
           <div
-            ref={moverRef}
-            className="outline outline-1 outline-slate-600/10 bg-borderLight rounded-full h-full p-2 transition-all w-0 duration-300"
-          ></div>
+            ref={navRef}
+            className={`max-w-50 m-auto h-full rounded-full border border-borderLight p-1 flex relative group transition-all ${
+              scrolled ? "shadow-md backdrop-blur bg-slate-600/10" : ""
+            }`}
+          >
+            <div className="opacity-0 group-hover:opacity-100 absolute w-full h-full top-0 left-0 p-1 -translate-x-1 transition-opacity">
+              <div
+                ref={moverRef}
+                className="outline outline-1 outline-slate-600/10 bg-borderLight rounded-full h-full p-2 transition-all w-0 duration-300"
+              ></div>
+            </div>
+            <div className="flex items-center justify-center gap-2 z-10 overflow-hidden">
+              {links.map((link, index) => (
+                <Link
+                  href={link.href}
+                  className="border-borderLight h-full flex justify-center items-center py-2 px-4 rounded-full transition-colors text-slate-300"
+                  onMouseOver={(ev) => handleMouseOver(ev)}
+                  key={index}
+                >
+                  {link.name}
+                </Link>
+              ))}
+            </div>
+          </div>
         </div>
-        <div className="flex items-center justify-center gap-2 z-10 overflow-hidden">
-          {links.map((link, index) => (
-            <Link
-              href={link.href}
-              className="border-borderLight h-full flex justify-center items-center py-2 px-4 rounded-full transition-colors text-slate-300"
-              onMouseOver={(ev) => handleMouseOver(ev)}
-              key={index}
-            >
-              {link.name}
-            </Link>
-          ))}
-        </div>
+        
+        {/* <button className={`transition-all duration-300 ${scrolled ? "translate-x-4 opacity-0" : ""}`}>Newsletter</button> */}
       </div>
-      {/* <button className={`transition-all duration-300 ${scrolled ? "translate-x-4 opacity-0" : ""}`}>Newsletter</button> */}
     </nav>
   );
 };
